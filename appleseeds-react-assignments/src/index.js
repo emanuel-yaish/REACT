a746174a5aad260d3d463653dba1bbb0527ce689
Emanuel Yaish <emanuelyaish@gmail.com>
2021-12-20 01:29:51 +0200

    fix num of exercise 13.2

--- appleseeds-react-assignments/src/index.js
+++ appleseeds-react-assignments/src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from "./reportWebVitals";
 // import App from "./components/7.3/App/App";
 // import App from "./components/11.1/App/App";
 // import App from "./components/12.1/App/App";
-import App from "./components/13.1/App/App";
+import App from "./components/13.2/App/App";
 
 // import App from "./components/11.2/App/App";
 // import App from "./components/8.1/App/App";
